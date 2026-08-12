a5aef2e2d9c72d31034f183712c0ec04f993106c
rushevich <george@rushevich.com>
2026-08-12 14:43:01 -0400

    chore: cleaned up org-mode configuration

--- emacs/.emacs.d/rush-emacs-modules/rush-emacs-org.el
+++ emacs/.emacs.d/rush-emacs-modules/rush-emacs-org.el
@@ -1,10 +1,20 @@
-(use-package org)
+(use-package org
+  :ensure nil
+  :config
+  
+  ;; General configuration
+  (setq org-directory "~/org/")
+  (setq org-agenda-files (list org-directory))
+  (add-to-list 'org-agenda-files (expand-file-name "~/.config/emacs/todo.org"))
+  (setq org-M-RET-may-split-line '((default . nil)))
+  (setq org-insert-heading-respect-content t)
+  (setq org-hide-emphasis-markers t)
 
-(setq org-directory "~/org/")
-(setq org-agenda-files (list org-directory))
-(setq org-M-RET-may-split-line '((default . nil)))
-(setq org-insert-heading-respect-content t)
-(setq org-log-done 'time)
-(setq org-log-into-drawer t)
-
+  ;; TODO configuration
+  (setq org-log-done 'time)
+  ;; By default, this will log into the LOGBOOK drawer
+  (setq org-log-into-drawer t)
+  (setq org-todo-keywords
+	'((sequence "TODO(t)" "WAIT(w!)" "|" "CANCELED(c)" "DONE(d)"))))
+  
 (provide 'rush-emacs-org)
