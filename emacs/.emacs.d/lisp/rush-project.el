40a19fc0f1c9c5eb36272fea43f7aee157f9c9f5
George Rushevich <george@rushevich.com>
2026-08-28 15:50:30 -0400

    feat: cmake project interactive commands

--- emacs/.emacs.d/lisp/rush-project.el
+++ emacs/.emacs.d/lisp/rush-project.el
@@ -11,6 +11,44 @@
 
 ;; This line ensures that C++ projects via CMake can be detected by project.el
 (add-to-list 'project-vc-extra-root-markers "CMakeLists.txt")
-;; TODO: add compiling and building ergonomics as well as project.el configuration
+
+;; TODO: follow DRY principle
+(defun rush/cmake-configure-project ()
+  "Configures the cmake project using the current project (as in project.el) root as the root directory
+If a CMakeLists.txt is not found within the root directory, or if there is no project, this function produces an error message."
+  (interactive)
+  (if (eq (project-current) nil) ;; then
+      (error "Current file does not refer to any project" )
+    (let*
+        ((root-dir (project-root (project-current)))
+         (default-directory root-dir))
+      (if (file-exists-p "CMakeLists.txt")
+          (async-shell-command "cmake -B build")
+        (error (format "Unable to find CMakeLists.txt in project root: %s" root-dir))))))
+
+(defun rush/cmake-build-project ()
+  "Builds the cmake project using the current project root as the root directory.
+If a CMakeLists.txt is not found within the root directory, or if there is no project, this function produces an error message."
+  (interactive)
+  (if (eq (project-current) nil) ;; then
+      (error "Current file does not refer to any project" )
+    (let*
+        ((root-dir (project-root (project-current)))
+         (default-directory root-dir))
+      (if (file-exists-p "CMakeLists.txt")
+          (async-shell-command "cmake --build build")
+        (error (format "Unable to find CMakeLists.txt in project root: %s" root-dir))))))
+
+(defun rush/cmake-build-and-configure-project ()
+  "Configures then builds the CMake project "
+  (interactive)
+    (if (eq (project-current) nil) ;; then
+      (error "Current file does not refer to any project" )
+    (let*
+        ((root-dir (project-root (project-current)))
+         (default-directory root-dir))
+      (if (file-exists-p "CMakeLists.txt")
+          (async-shell-command "cmake -B build && cmake --build build")
+        (error (format "Unable to find CMakeLists.txt in project root: %s" root-dir))))))
 
 (provide 'rush-project)
